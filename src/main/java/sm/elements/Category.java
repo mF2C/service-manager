989b80dcc6d658f0d02f3f612b4f62999a7978c5
Fran Carpio <carpiofj@gmail.com>
2018-07-03 12:23:21 +0200

    Updated use-case 1 services. Now, the specification of not used sensors is not required.

--- src/main/java/sm/elements/Category.java
+++ src/main/java/sm/elements/Category.java
@@ -9,11 +9,9 @@
 package sm.elements;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Category {
 
     private String cpu;
