030885bdfeb1417770f45be5782b9f0a9978b2d4
Francisco Carpio <carpiofj@gmail.com>
2018-11-30 16:41:32 +0100

    updated GUI for new service definition

--- src/main/resources/static/scripts/js/service_catalog.js
+++ src/main/resources/static/scripts/js/service_catalog.js
@@ -1,3 +1,51 @@
+window.onload=function(){
+	var services = getServices()['services'];
+	var catalog = document.getElementById('catalog');
+	var r=0;
+	var width = $(window).width();
+	var comlumns = 4;
+	var columnSize = "l3"
+	if(width<1080){
+		comlumns = 2;
+		columnSize = "m6"
+	}
+	if(width<600){
+		comlumns = 1;
+		columnSize = "s12"
+	}
+	var rows = Math.floor(services.length/comlumns);
+	var remainder = services.length % comlumns;
+	var counter = 0;
+	while(r<rows){
+		var c=0;
+		createRow(catalog, r);
+		while(c<comlumns){
+			var service = services[counter];
+			var id = r+""+c;
+			createColumn(catalog, id, r, service, columnSize);
+			c++;
+			counter++;
+		}
+		r++;
+	}
+	if(remainder>0){
+		var c=0;
+		createRow(catalog, r);
+		while(c<remainder){
+			var service = services[counter];
+			var id = r+""+c;
+			createColumn(catalog, id, r, service, columnSize);
+			c++;
+			counter++;
+		}
+	}
+    var ele = document.createElement("button");
+	ele.setAttribute("class","button button-register");
+	ele.setAttribute("onClick","location.href='registration.html'");
+	ele.innerHTML="New service";
+	catalog.appendChild(ele);
+};
+
 function getServices()
 {
 	try
@@ -5,9 +53,9 @@ function getServices()
 		var message = null;
 		$.ajax
 		({
-			url:   "../php/cimi_get_services.php",
+			url:   "http://localhost:46200/api/service-management",
 			type:  "GET",
-			async: false, 
+			async: false,
 			success: function(ans)
 			{
 				message = ans;
@@ -16,11 +64,6 @@ function getServices()
 		return message;
 	}
 	catch (e){return 0;}
-}
-
-function goBack()
-{
-	window.location = '../../../index.php';
 }
 
 function createServiceInstance(serviceObject)
@@ -129,61 +172,6 @@ function deleteService(service)
 	catch (e){return 0;}
 }
 
-window.onload=function(){
-
-	var servicesJson = getServices();
-	var services = JSON.parse(servicesJson);
-	var catalog = document.getElementById('catalog');
-	var r=0;
-	var width = $(window).width();
-	var comlumns = 4;
-	var columnSize = "l3"
-	if(width<1080){
-		comlumns = 2;
-		columnSize = "m6"
-	}
-	if(width<600){
-		comlumns = 1;
-		columnSize = "s12"
-	}
-	var rows = Math.floor(services.length/comlumns);
-	var remainder = services.length % comlumns;
-	var counter = 0;
-
-	while(r<rows){
-
-		var c=0;
-		createRow(catalog, r);
-		
-		while(c<comlumns){
-			var service = services[counter];
-			var id = r+""+c;
-			createColumn(catalog, id, r, service, columnSize);
-			c++;
-			counter++;
-		}
-		r++;
-	}
-
-	if(remainder>0){
-		var c=0;
-		createRow(catalog, r);
-		while(c<remainder){
-			var service = services[counter];
-			var id = r+""+c;
-			createColumn(catalog, id, r, service, columnSize);
-			c++;
-			counter++;
-		}
-	}
-
-	var ele = document.createElement("button");
-	ele.setAttribute("class","button button-back");
-	ele.setAttribute("onClick","goBack()");
-	ele.innerHTML="Back";
-	catalog.appendChild(ele);
-};
-
 function createRow(catalog, id){
 
 	var ele = document.createElement("div");
@@ -218,8 +206,9 @@ function createColumn(catalog, id, r, service, columnSize){
 	var title = document.createElement("h2");
 	title.setAttribute("id","title"+id);
 	var titleString;
-	if(service['name'].length > 19){
-		titleString = service['name'].substring(0, 20) + " ...";
+	var maxLength = 15;
+	if(service['name'].length > maxLength){
+		titleString = service['name'].substring(0, maxLength) + " ...";
 	}
 	else{
 		titleString = service['name'];
@@ -238,26 +227,36 @@ function createColumn(catalog, id, r, service, columnSize){
 	document.getElementById("content"+id).appendChild(section);
 
 	var description = document.createElement("h6");
-	description.setAttribute("id","h6-description-"+id);
+	description.setAttribute("id","description-"+id);
 	description.innerHTML= service['description'];
 	document.getElementById("content"+id).appendChild(description);
 
     var type = document.createElement("h6");
-	type.setAttribute("id","h6-type-"+id);
-	type.innerHTML= service['exec_type'];
+    type.setAttribute("id","exec-"+id);
+    type.innerHTML= "exec: " + service['exec'];
+    document.getElementById("content"+id).appendChild(type);
+
+    var type = document.createElement("h6");
+	type.setAttribute("id","exec_type-"+id);
+	type.innerHTML= "exec_type: " + service['exec_type'];
 	document.getElementById("content"+id).appendChild(type);
+
+    var type = document.createElement("h6");
+    type.setAttribute("id","agent_type-"+id);
+    type.innerHTML= "agent_type: " + service['agent_type'];
+    document.getElementById("content"+id).appendChild(type);
 
 	var launchButton = document.createElement("button");
 	launchButton.setAttribute("id","button_launch"+id);
 	launchButton.setAttribute("class","button button-launch");
-	launchButton.onclick = function() {  createServiceInstance(service);}
+	launchButton.onclick = function() {createServiceInstance(service);}
 	launchButton.innerHTML="Launch";
 	document.getElementById("card"+id).appendChild(launchButton);
 
 	var deleteButton = document.createElement("button");
 	deleteButton.setAttribute("id","button_delete"+id);
 	deleteButton.setAttribute("class","button button-delete");
-	deleteButton.onclick = function() {  deleteService(service);  }
+	deleteButton.onclick = function() {deleteService(service);}
 	deleteButton.innerHTML="Delete";
 	document.getElementById("card"+id).appendChild(deleteButton);
 }
--- src/main/resources/static/scripts/php/get_user.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$user = $_SESSION['mF2C_user'];
-echo $user;
-?>
